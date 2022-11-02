e13fadf92073bee94b72d0f07755d57a908aa876
serpentaire <glemoine@hotmail.fr>
2022-11-02 22:18:26 +0100

    Solution quete Express 02

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+// app.get("/api/movies", movieHandlers.getMovies);
+// app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", movieHandlers.getUsers);
+app.get("/api/users/:id", movieHandlers.getUsersById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
---
+++ movieHandlers.js
@@ -0,0 +1,98 @@
+// const database = require("./database");
+const databaseuser = require("./database");
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    colors: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    colors: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+// const getMovies = (req, res) => {
+//   database
+//     .query("select * from movies")
+//     .then(([movies]) => {
+//       res.json(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error retrieving data from database");
+//     });
+// };
+
+// const getMovieById = (req, res) => {
+//   const id = parseInt(req.params.id);
+
+//   database
+//   .query("select * from movies where id = ?", [id])
+//   .then(([movies]) => {
+//     if (movies[0] != null) {
+//       res.json(movies[0]);
+//     } else {
+//       res.status(404).send("Not Found");
+//     }
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send("Error retrieving data from database");
+//   });
+// };
+
+const getUsers = (req, res) => {
+  databaseuser
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendstatus(200);
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  databaseuser
+  .query("select * from users where id = ?", [id])
+  .then(([users]) => {
+    if (users[0] != null) {
+      res.json(users[0]);
+    } else {
+      res.status(404).send("Not Found");
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error retrieving data from database");
+  });
+};
+
+// module.exports = {
+//   getMovies,
+//   getMovieById,
+// };
+module.exports = {
+  getUsers,
+  getUsersById,
+};
