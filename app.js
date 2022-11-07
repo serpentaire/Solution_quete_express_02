0de7a40ef6017bc642b4228b7ab459d0ab820806
serpentaire <glemoine@hotmail.fr>
2022-11-07 12:10:44 +0100

    solution quete express 05

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", userHandlers.postUser);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
