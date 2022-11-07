6c047839543094edb9569392760cdaf131f2d03b
serpentaire <glemoine@hotmail.fr>
2022-11-07 11:22:48 +0100

    solution quete express 03

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -18,6 +21,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUsersById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
